9ace4e0ecf3eebce6ca4be56d7ba01c51678a3b0
Hemendra1234 <hemendra@innovaeps.com>
2020-12-30 10:39:05 +0530

    For demo

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.get('/', function(request, response) {
 
 // Create an http server and run it
 const server = http.createServer(app);
-const port = process.env.PORT || 7000;
+const port = process.env.PORT || 8000;
 server.listen(port, function() {
   console.log('Express server running on *:' + port);
 });
